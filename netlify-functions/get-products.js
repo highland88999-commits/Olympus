9578e33e3a4904c826ebb7dda46608cdf7fffb11
highland88999-commits <highland88999@gmail.com>
2025-12-22 14:43:32 -0700

    Update get-products.js

--- netlify-functions/get-products.js
+++ netlify-functions/get-products.js
@@ -1,29 +1,27 @@
-const fetch = require('node-fetch');
-
 exports.handler = async (event, context) => {
-  const PRINTFUL_API_KEY = process.env.PRINTFUL_API_KEY;
+  const API_KEY = process.env.PRINTFUL_API_KEY;
 
   try {
-    // We pull from /product-templates to get ALL 71 pages of designs
-    // even the ones not synced to Etsy
-    const response = await fetch('https://api.printful.com/product-templates?limit=100', {
-      headers: { 'Authorization': `Bearer ${PRINTFUL_API_KEY}` }
+    // 1. Get the list of 586 templates
+    const listRes = await fetch('https://api.printful.com/product-templates?limit=100', {
+      headers: { 'Authorization': `Bearer ${API_KEY}` }
     });
+    const listData = await listRes.json();
     
-    const data = await response.json();
-    const allTemplates = data.result?.items || [];
-
-    const products = allTemplates.map(item => ({
-      id: item.id,
-      name: item.title, 
-      // Templates use thumbnail_url for the high-res mockup design
-      images: [item.thumbnail_url || "https://www.printful.com/static/images/layout/default-product-image.png"],
-      price: "95.00" 
-    }));
+    // 2. Map them into your Axiom Repository format
+    const products = listData.result.items.map(template => {
+      return {
+        id: template.id,
+        name: template.title,
+        // Using the Template's thumbnail ensures pocket alignment is preserved
+        images: [template.thumbnail_url], 
+        price: "95.00"
+      };
+    });
 
     return {
       statusCode: 200,
-      headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+      headers: { "Access-Control-Allow-Origin": "*" },
       body: JSON.stringify(products),
     };
   } catch (error) {
