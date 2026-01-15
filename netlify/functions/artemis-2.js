6953ec401ce3b90eca6b13ec59de69ecfcf901cb
highland88999-commits <highland88999@gmail.com>
2026-01-15 08:46:39 -0700

    Update artemis-2.js

--- netlify/functions/artemis-2.js
+++ netlify/functions/artemis-2.js
@@ -1,13 +1,6 @@
-// netlify/functions/artemis-2.js
 const manifest = require('./archive-map.json');
-
-exports.handler = async () => {
-    return {
-        statusCode: 200,
-        headers: { 
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*" 
-        },
-        body: JSON.stringify(manifest)
-    };
-};
+exports.handler = async () => ({
+    statusCode: 200,
+    headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+    body: JSON.stringify(manifest)
+});
